663058f8a2b4ff00c116c63e693ec3cd3c8f2768
Sharanya1998 <54271316+Sharanya1998@users.noreply.github.com>
2019-09-30 15:09:21 +0530

    Add files via upload

---
+++ 7.php
@@ -0,0 +1,13 @@
+<?php
+
+
+
+date_default_timezone_set('Asia/Kolkata');
+
+
+
+echo date('d/m/Y==h:i:s A');
+
+
+
+?>
---
+++ prog6.php
@@ -0,0 +1,11 @@
+<?php
+	$fp=fopen("cntlog.txt","r");
+	$count=fread($fp,10);
+	fclose($fp);
+	$count=$count+1;
+	echo "page views: ".$count."</p>";
+	$fp=fopen("cntlog.txt","w");
+	fwrite($fp,$count);
+	fclose($fp);
+?>
+
